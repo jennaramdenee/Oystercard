f84bd9a3a7d13c8c03bfb87ae24ec7445b549e45
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-15 11:20:26 +0000

    Initialized card with balance

---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,23 @@
+require './lib/oystercard.rb'
+
+describe Oystercard do
+
+   it "Initializes card with zero balance" do
+     expect(subject.balance).to eq 0
+   end
+
+
+
+
+
+
+
+
+end
+
+
+
+
+#unitialized constant
+#Users/jennaramdenee/Documents/Week_2/Oystercard/spec/oystercard_spec.rb
+#:3
