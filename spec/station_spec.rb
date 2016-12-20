228f0d8c17c211bd5070e5b0a8f6558ca24908b5
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-20 13:16:44 +0000

    Changed type of zone input from string to integer

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,16 +2,16 @@ require './lib/station.rb'
 
 describe Station do
 
-before(:each) do
-  @hammersmith = Station.new("Hammersmith", "Zone 2")
-end
+  before(:each) do
+    @hammersmith = Station.new("Hammersmith", 2)
+  end
 
-  it "will be able to show the station name" do
+  it "can show the station name" do
     expect(@hammersmith.name).to eq("Hammersmith")
   end
 
-  it "will be able to show the station's zone" do
-    expect(@hammersmith.zone).to eq("Zone 2")
+  it "can show the station's zone" do
+    expect(@hammersmith.zone).to eq(2)
   end
 
 
