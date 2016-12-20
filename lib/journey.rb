625a2fbc45f32a81438f4303a677d0585af6f884
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-20 13:15:37 +0000

    Removed logging entry and exit stations from Class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,30 +1,9 @@
 class Journey
 
-  attr_reader :entry_station, :exit_station, :entry_station, :journey_store
+  attr_reader :entry_station, :exit_station
 
     MINIMUM_FARE = 1
     PENALTY_FARE = 6
-
-  def initialize
-    @entry_station
-    @exit_station
-  end
-
-  def in_journey?
-    !@exit_station
-  end
-
-  def touch_in(entry_station)
-    @entry_station = entry_station
-  end
-
-  def touch_out(exit_station)
-    @exit_station = exit_station
-  end
-
-  def completed?
-    !!@exit_station && !!@entry_station
-  end
 
   def fare
     return PENALTY_FARE if completed? == false
