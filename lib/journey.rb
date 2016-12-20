a38d67312ff5e5494bbc95ad8afdbd72416222be
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-20 15:36:44 +0000

    Added feature to detect incomplete journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -8,18 +8,18 @@ class Journey
   end
 
   def current_journey
-    if @single_journey.has_key?(:entry_station) && @single_journey.has_key?(:exit_station)
-      add_to_history
-    elsif @single_journey.has_key?(:exit_station) == false
-      @single_journey[:exit_station] = "No touch out"
-      add_to_history
-    elsif @single_journey.has_key?(:entry_station) == false
-      @single_journey[:entry_station] = "No touch in"
+    unless @single_journey == {}
+      if @single_journey.has_key?(:exit_station) == false
+        @single_journey[:exit_station] = "No touch out"
+      elsif @single_journey.has_key?(:entry_station) == false
+        @single_journey[:entry_station] = "No touch in"
+      end
       add_to_history
     end
   end
 
   def start(entry_station)
+    current_journey
     @single_journey[:entry_station] = entry_station
   end
 
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -6,7 +6,7 @@ describe Journey do
   let(:entry_station) { double(:entry_station) }
   let(:exit_station) { double(:exit_station) }
 
-  context "initializing" do
+  context "Initializing" do
     it "should initialize an empty single journey" do
       expect(journey.single_journey).to eq({})
     end
@@ -22,7 +22,7 @@ describe Journey do
     end
   end
 
-  context "End Journey" do
+  context "Finish Journey" do
     it "should log exit station as part of single journey" do
       journey.finish(exit_station)
       expect(journey.single_journey[:exit_station]).to eq exit_station
@@ -36,6 +36,75 @@ describe Journey do
       journey.add_to_history
       expect(journey.journey_history).to eq [{:entry_station => entry_station, :exit_station => exit_station}]
     end
+    it "should clear single journey hash" do
+      journey.start(entry_station)
+      journey.finish(exit_station)
+      journey.add_to_history
+      expect(journey.single_journey).to eq ({})
+    end
+    it "can add multiple journeys to history" do
+      journey.start(entry_station)
+      journey.finish(exit_station)
+      journey.start(entry_station)
+      journey.finish(exit_station)
+      journey.add_to_history
+      expect(journey.journey_history).to eq ([{:entry_station => entry_station, :exit_station => exit_station}, {:entry_station => entry_station, :exit_station => exit_station}])
+    end
   end
 
+  # context "Incomplete Journeys" do
+  #   it "should be able to add to journey when user has not touched in" do
+  #     journey.finish(exit_station)
+  #     journey.current_journey
+  #     expect(journey.single_journey[:entry_station]).to eq "No touch in"
+  #   end
+  #
+  #   it "should be able to add to journey when user has not touched out" do
+  #     journey.start(entry_station)
+  #     journey.current_journey
+  #     expect(journey.single_journey[:exit_station]).to eq "No touch out"
+  #   end
+  #
+  #   it "should be able to detect when user has touched both in and out" do
+  #     journey.start(entry_station)
+  #     journey.finish(exit_station)
+  #     journey.current_journey
+  #     expect(journey.single_journey).to eq ({:entry_station => entry_station, :exit_station => exit_station})
+  #   end
+  #
+  #   it "should do nothing when a new journey is started" do
+  #     journey.current_journey
+  #     expect(journey.single_journey).to eq ({})
+  #   end
+  #
+  # end
+
+  context "Updating journey history" do
+    it "can update journey history with incomplete end journey" do
+      journey.start(entry_station)
+      journey.start(entry_station)
+      expect(journey.journey_history).to eq ([{:entry_station => entry_station, :exit_station => "No touch out"}])
+    end
+    it "can update journey history with incomplete start journey" do
+      journey.finish(exit_station)
+      journey.start(entry_station)
+      expect(journey.journey_history).to eq ([{:entry_station => "No touch in", :exit_station => exit_station}])
+    end
+    it "can update journey history with complete journey" do
+      journey.start(entry_station)
+      journey.finish(exit_station)
+      journey.start(entry_station)
+      expect(journey.journey_history).to eq ([{:entry_station => entry_station, :exit_station => exit_station}])
+    end
+    it "does not add to history at creation" do
+      journey.start(entry_station)
+      expect(journey.journey_history).to eq ([])
+    end
+    it "only adds to history once the next journey has started" do
+      journey.start(entry_station)
+      journey.finish(exit_station)
+      expect(journey.journey_history).to eq ([])
+    end
+
+  end
 end
